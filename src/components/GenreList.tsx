936b93cbfd168a6ab91c525dd3378c0adeb1e697
Shubam Sharma <shubamuzumaki@gmail.com>
2024-05-13 23:19:44 +0530

    added generic hook

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import useGenre from '../hooks/useGenre'
 
 const GenreList = () => {
-  const { genres } = useGenre()
+  const { data } = useGenre()
   return (
     <ul>
-      {genres.map((genre) => (
+      {data.map((genre) => (
         <li key={genre.id}>{genre.name}</li>
       ))}
     </ul>
