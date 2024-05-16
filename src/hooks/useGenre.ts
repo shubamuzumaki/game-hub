0615069dad4d7b70da2b69df04257cead9f7bd4a
Shubam Sharma <shubamuzumaki@gmail.com>
2024-05-16 23:06:38 +0530

    refactored game grid

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,11 +1,11 @@
 import useData from './useData'
-
+import genre from '../data/genre'
 export interface Genre {
   id: number
   name: string
   image_background: string
 }
 
-const useGenre = () => useData<Genre>('/genres')
+const useGenre = () => ({ data: genre, error: null, isLoading: null })
 
 export default useGenre
